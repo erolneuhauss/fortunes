ad2690b2684667cbc056c1993ea76b07c184f75d
eneuhauss <erol.neuhauss@gmx.de>
2021-03-22 22:58:42 +0100

    progress

--- update_fortune.sh
+++ update_fortune.sh
@@ -1,6 +1,9 @@
-/usr/bin/env bash
+#!/usr/bin/env bash
 
 for file in erol erol.dat; do
   strfile erol
   sudo install -o root -g root -m 0644 $file /usr/share/games/fortunes/$file
 done
+git add erol erol.dat update_fortune.sh
+git commit --message 'progress'
+git push
